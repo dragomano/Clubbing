0cfd989cc87c92945dd0d7797aa1be9fc7aef4c4
Bugo <bugo@dragomano.ru>
2018-05-28 19:50:58 +0500

    Оптимизируем код

--- Sources/Class-Clubbing.php
+++ Sources/Class-Clubbing.php
@@ -18,15 +18,15 @@ class Clubbing
 {
 	public static function hooks()
 	{
-		add_integration_function('integrate_load_theme', 'Clubbing::load_theme', false);
+		add_integration_function('integrate_load_theme', 'Clubbing::loadTheme', false);
 		add_integration_function('integrate_actions', 'Clubbing::actions', false);
-		add_integration_function('integrate_load_permissions', 'Clubbing::load_permissions', false);
-		add_integration_function('integrate_display_buttons', 'Clubbing::display_buttons', false);
-		add_integration_function('integrate_prepare_display_context', 'Clubbing::prepare_display_context', false);
-		add_integration_function('integrate_profile_areas', 'Clubbing::profile_areas', false);
+		add_integration_function('integrate_load_permissions', 'Clubbing::loadPermissions', false);
+		add_integration_function('integrate_display_buttons', 'Clubbing::displayButtons', false);
+		add_integration_function('integrate_prepare_display_context', 'Clubbing::prepareDisplayContext', false);
+		add_integration_function('integrate_profile_areas', 'Clubbing::profileAreas', false);
 	}
 
-	public static function load_theme()
+	public static function loadTheme()
 	{
 		global $context, $settings;
 
@@ -38,20 +38,102 @@ class Clubbing
 			return;
 
 		$context['html_headers'] .= '
-		<link rel="stylesheet" type="text/css" href="' . $settings['default_theme_url'] . '/css/clubbing/iziModal.min.css" />
-		<link rel="stylesheet" type="text/css" href="' . $settings['default_theme_url'] . '/css/clubbing/clubbing.css" />';
+	<link rel="stylesheet" type="text/css" href="' . $settings['default_theme_url'] . '/css/clubbing/iziModal.min.css" />
+	<link rel="stylesheet" type="text/css" href="' . $settings['default_theme_url'] . '/css/clubbing/clubbing.css" />';
 
 		$context['insert_after_template'] .= '
-			<script type="text/javascript" src="' . $settings['default_theme_url'] . '/scripts/clubbing/jquery.min.js"></script>
-			<script type="text/javascript" src="' . $settings['default_theme_url'] . '/scripts/clubbing/iziModal.min.js"></script>';
+		<script type="text/javascript" src="' . $settings['default_theme_url'] . '/scripts/clubbing/jquery.min.js"></script>
+		<script type="text/javascript" src="' . $settings['default_theme_url'] . '/scripts/clubbing/iziModal.min.js"></script>';
 	}
 
 	public static function actions(&$action_array)
 	{
-		$action_array['clubbings'] = array('Subs-Clubbing.php', 'clubbingActions');
+		$action_array['clubbings'] = array('Class-Clubbing.php', array('Clubbing', 'subactions'));
 	}
 
-	public static function load_permissions(&$permissionGroups, &$permissionList)
+	public static function subactions()
+	{
+		$subActions = array(
+			'add'  => array('Clubbing', 'addClubbing'),
+			'edit' => array('Clubbing', 'editClubbing')
+		);
+
+		if (isset($_REQUEST['sa']) && isset($subActions[$_REQUEST['sa']]))
+			return call_user_func($subActions[$_REQUEST['sa']]);
+	}
+
+	private static function addClubbing()
+	{
+		global $context, $sourcedir, $smcFunc;
+
+		isAllowedTo('make_clubbings');
+
+		loadTemplate('Clubbing');
+		$context['sub_template'] = 'post';
+
+		if (empty($_POST))
+			return;
+
+		require_once($sourcedir . '/Subs-Editor.php');
+
+		if (isset($_POST['requisites'])) {
+			$_POST['requisites'] = un_htmlspecialchars($_POST['requisites']);
+
+			censorText($_POST['requisites']);
+
+			$smcFunc['db_insert']('',
+				'{db_prefix}cb_items',
+				array(
+					'topic_id'   => 'int',
+					'price'      => 'float',
+					'currency'   => 'string-3',
+					'requisites' => 'string'
+				),
+				array(
+					(int) $_POST['topic'],
+					(float) $_POST['price'],
+					(string) $_POST['currency'],
+					$smcFunc['htmlspecialchars']($_POST['requisites'], ENT_QUOTES)
+				),
+				array('id')
+			);
+		}
+	}
+
+	private static function editClubbing()
+	{
+		global $context, $sourcedir, $smcFunc;
+
+		isAllowedTo('make_clubbings');
+
+		loadTemplate('Clubbing');
+		$context['sub_template'] = 'post';
+
+		if (empty($_POST))
+			return;
+
+		require_once($sourcedir . '/Subs-Editor.php');
+
+		if (isset($_POST['requisites'])) {
+			$_POST['requisites'] = un_htmlspecialchars($_POST['requisites']);
+
+			censorText($_POST['requisites']);
+
+			$smcFunc['db_query']('', '
+				UPDATE {db_prefix}cb_items
+				SET	price = {float:price}, currency = {string:currency}, requisites = {string:requisites}
+				WHERE topic_id = {int:topic}',
+				array(
+					'topic'      => (int) $_POST['topic'],
+					'price'      => (float) $_POST['price'],
+					'currency'   => (string) $_POST['currency'],
+					'requisites' => $smcFunc['htmlspecialchars']($_POST['requisites'], ENT_QUOTES)
+				)
+			);
+		}
+	}
+
+	public static function loadPermissions(&$permissionGroups, &$permissionList)
 	{
 		global $context;
 
@@ -63,7 +145,7 @@ class Clubbing
 		$context['non_guest_permissions'] = array_merge($context['non_guest_permissions'], array('make_clubbings'));
 	}
 
-	public static function display_buttons(&$normal_buttons)
+	public static function displayButtons(&$normal_buttons)
 	{
 		global $context, $user_info, $sourcedir, $smcFunc;
 
@@ -124,7 +206,7 @@ class Clubbing
 		$context['template_layers'][] = 'display';
 	}
 
-	public static function prepare_display_context(&$output, &$message)
+	public static function prepareDisplayContext(&$output, &$message)
 	{
 		global $context, $smcFunc, $sourcedir, $txt, $boardurl, $user_info, $scripturl;
 
@@ -216,13 +298,12 @@ class Clubbing
 		}
 	}
 
-	public static function profile_areas(&$profile_areas)
+	public static function profileAreas(&$profile_areas)
 	{
 		global $txt;
 
 		$profile_areas['info']['areas']['clubbings'] = array(
 			'label'       => $txt['cb_clubbings'],
-			'file'        => 'Subs-Clubbing.php',
 			'function'    => 'clubbingProfile',
 			'subsections' => array(),
 			'permission'  => array(
@@ -231,4 +312,139 @@ class Clubbing
 			),
 		);
 	}
+}
+
+function clubbingProfile($memID)
+{
+	global $context, $txt, $scripturl, $user_info, $user_profile, $smcFunc;
+
+	$context['start']          = (int) $_REQUEST['start'];
+	$context['current_member'] = $memID;
+
+	loadLanguage('Modlog');
+
+	$context[$context['profile_menu_name']]['tab_data'] = array(
+		'title' => $txt['cb_clubbings'],
+		'icon'  => 'profile_sm.gif',
+		'tabs'  => array()
+	);
+
+	$num = 10;
+
+	$request = $smcFunc['db_query']('', '
+		SELECT COUNT(id)
+		FROM {db_prefix}cb_items',
+		array()
+	);
+
+	list ($total) = $smcFunc['db_fetch_row']($request);
+	$smcFunc['db_free_result']($request);
+
+	$context['page_index'] = constructPageIndex($scripturl . '?action=profile;u=' . $memID . ';area=clubbings', $context['start'], $total, $num);
+
+	$start   = $context['start'];
+	$reverse = $context['start'] > $num / 2;
+	if ($reverse) {
+		$num   = $total < $context['start'] + $num + 1 && $total > $context['start'] ? $total - $context['start'] : $num;
+		$start = $total < $context['start'] + $num + 1 || $total < $context['start'] + $num ? 0 : $total - $context['start'] - $num;
+	}
+	$counter = $reverse ? $context['start'] + $num + 1 : $context['start'];
+
+	$request = $smcFunc['db_query']('', '
+		SELECT c.id, c.price, c.requisites, t.id_topic, m.subject, m.body, m.poster_time
+		FROM {db_prefix}cb_items AS c
+			INNER JOIN {db_prefix}topics AS t ON (t.id_topic = c.topic_id)
+			INNER JOIN {db_prefix}messages AS m ON (m.id_msg = t.id_first_msg)
+		WHERE t.id_member_started = {int:current_user}
+		ORDER BY m.poster_time ' . ($reverse ? 'ASC' : 'DESC') . '
+		LIMIT {int:start}, {int:num}',
+		array(
+			'current_user' => $context['current_member'],
+			'start'        => $start,
+			'num'          => $num
+		)
+	);
+
+	$topics = [];
+	while ($row = $smcFunc['db_fetch_assoc']($request))	{
+		censorText($row['subject']);
+
+		$topics[] = $row['id_topic'];
+		$context['items'][$counter += $reverse ? -1 : 1] = array(
+			'id'         => $row['id'],
+			'counter'    => $counter,
+			'alternate'  => $counter % 2,
+			'topic'      => $row['id_topic'],
+			'title'      => $row['subject'],
+			'date'       => timeformat($row['poster_time']),
+			'text'       => parse_bbc($row['body']),
+			'requisites' => parse_bbc($row['requisites'])
+		);
+	}
+
+	$smcFunc['db_free_result']($request);
+
+	$request = $smcFunc['db_query']('', '
+		SELECT topic_id, IFNULL(member_name, 0) AS member_name
+		FROM {db_prefix}cb_members
+			INNER JOIN {db_prefix}members ON (id_member = member_id)
+		WHERE topic_id IN ({array_int:topics})',
+		array(
+			'topics' => $topics
+		)
+	);
+
+	$context['clubbing_members'] = [];
+	while ($row = $smcFunc['db_fetch_assoc']($request))
+		$context['clubbing_members'][$row['topic_id']][] = $row['member_name'];
+
+	$smcFunc['db_free_result']($request);
+
+	if ($reverse)
+		$context['items'] = array_reverse($context['items'], true);
+
+	$context['cb_can_delete'] = $context['current_member'] == $user_info['id'];
+
+	$redirect = !empty($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '?action=profile;area=clubbings;u=' . $memID;
+
+	if ($context['cb_can_delete']) {
+		if (isset($_POST['new_member'])) {
+			$members = explode(',', trim($_POST['new_member'], ' '));
+
+			foreach ($members as $member) {
+				$smcFunc['db_insert']('replace',
+					'{db_prefix}cb_members',
+					array(
+						'member_id' => 'int',
+						'topic_id'  => 'int'
+					),
+					array(
+						(int) $member,
+						(int) $_POST['topic']
+					),
+					array()
+				);
+			}
+		}
+
+		if (isset($_REQUEST['del_item'])) {
+			$smcFunc['db_query']('', '
+				DELETE FROM {db_prefix}cb_items
+				WHERE id = {int:item}',
+				array(
+					'item' => (int) $_REQUEST['del_item']
+				)
+			);
+
+
+			if (isset($_REQUEST['profile'], $_REQUEST['start'], $_REQUEST['u']))
+				redirectexit('action=profile;area=clubbings;start=' . $_REQUEST['start'] . ';u=' . $_REQUEST['u']);
+			else
+				redirectexit($redirect);
+		}
+	}
+
+	loadTemplate('Clubbing');
+	$context['sub_template'] = 'profile';
+	$context['page_title']   = $txt['cb_clubbings'] . ' - ' . $user_profile[$memID]['real_name'];
 }
